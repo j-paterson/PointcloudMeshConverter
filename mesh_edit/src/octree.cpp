22e561433137471db97d9e075cfaf09d5faaafb6
Bryce Schmidtchen <BryceSchmidtchen@gmail.com>
2017-04-29 21:48:47 -0700

    more changes to octree

--- mesh_edit/src/octree.cpp
+++ mesh_edit/src/octree.cpp
@@ -1,16 +1,15 @@
 #include "octree.h"
 
 
-OctreeNode * OctreeNode::construct_ot(const std::vector<Vector3D> points, size_t max_depth) {
-  //take points and construct tree from the node.
-}
-
 //when we first call this consuctor, we will have an empty BBox so this constructor needs to
 OctreeNode::OctreeNode(std::vector<Point> points, int depth, Octree parent, BBox nodeBB, int maxDepth) {
 
   //if the depth is zero, expand the bbox etc but if not do other things
   this.depth = depth;
   this.maxDepth = maxDepth;
+  this.NodeBB = nodeBB;
+  this.parent = parent;
+
   if (this.depth == 0 && this.depth != maxDepth) {
     for (Point p : points) {
         NodeBB.expand(p.coordinates); //should expand the parent's bbox to include all points provide in our .ply.
@@ -19,16 +18,17 @@ OctreeNode::OctreeNode(std::vector<Point> points, int depth, Octree parent, BBox
 
     parent = nullptr;
 
-  } else if (this.depth = maxDepth){
+  } else if (this.depth = maxDepth) {
     this.isLeaf = true;
-
-
+    for (Point p : points) {
+        nodePoints.push_back(p);
+    }
   }
 
   this.Center = NodeBB.centroid();
 
   //generate the 8 inner children of the current node.
-  if (points.size() != 0) { //generate children when the current one isn't empty.
+  if (points.size() != 0 && this.depth != maxDepth) { //generate children when the current one isn't empty.
   //create 8 vectors of points and then call the function that splits the bboxs in 8. Then go through all points
   //in this current node, if one is in one of those bboxs then add it to the matching vector
   //after this process is complete for all points, we can call the consutrctors for these children
