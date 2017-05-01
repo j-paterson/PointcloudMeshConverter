748b7eb684c61c704d1f01da658ffdaaca109701
Jesse Paterson <jesse.r.pat@gmail.com>
2017-04-30 19:53:30 -0700

    Worked on marchingcubes some more, this implementation should find all the triangles and put them into a mesh struct

--- mesh_edit/src/marchingcubes.h
+++ mesh_edit/src/marchingcubes.h
@@ -1,11 +1,17 @@
 #ifndef MARCHINGCUBES_H
 #define MARCHINGCUBES_H
 
+typedef struct {
+    vector<Vector3D> points;
+} Triangle;
 
-class marchingCubes
-{
-public:
-    marchingCubes();
-};
+typedef struct{
+  vector<Triangle> triangles;
+  vector<Vector3D> points;
+  double price;
+} Mesh;
+
+Mesh marchingCubes(OctreeNode currentNode, vFunctionCall IndicatorFunction);
+
 
 #endif // MARCHINGCUBES_H
